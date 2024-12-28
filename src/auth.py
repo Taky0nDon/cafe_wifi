f1816c1b1bb4dee96b88e590c883de20b18cd165
Taky0ndon <mmaggio89@gmail.com>
2024-12-27 19:30:30 -0500

    added admin_only decorator and integrated flask_login

--- src/auth.py
+++ src/auth.py
@@ -1,13 +1,26 @@
-from flask import Blueprint, render_template, request, Flask, g
+import flask_login
+from flask import Blueprint, render_template, request, Flask, g, request, redirect, url_for, abort
+from functools import wraps
 
 
 from forms import LoginForm
 from DatabaseManager import get_db
+from UserManager import get_users, User
 
 auth = Blueprint('auth', __name__)
 app = Flask("app")
 with app.app_context():
     db = get_db()
+
+def admin_only(function):
+    @wraps(function)
+    def view(*args, **kwargs):
+        if flask_login.current_user.is_anonymous or flask_login.current_user.id != "ser03":
+            return abort(code=403)
+        else:
+            return function(*args, **kwargs)
+    return view
+
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -18,25 +31,15 @@ def close_connection(exception):
 @auth.route('/login', methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    if request.method == "POST":
-        print(request.form.get('username'), request.form.get('password'))
-        statement = "INSERT INTO user VALUES (?, ?, ?)"
-        c = db.cursor()
-        user_id = c.execute("SELECT Count(*) FROM user").fetchall()[0][0]
-        print(user_id)
-        c.execute(statement,
-                  [user_id, request.form.get('username'), request.form.get('password')]
-                  )
-        db.commit()
-        db.close()
+    if request.method == "GET":
+        return render_template("login.html", form=login_form)
 
+    username = request.form["username"]
+    users = get_users()
+    if username in users and request.form["password"] == users[username]["pw_hash"]:
+        user = User()
+        user.id = username
+        flask_login.login_user(user)
+        return redirect(url_for("admin_welcome"))
 
-    return render_template("login.html", form=login_form)
-
-@auth.route('/signup')
-def signup():
-    return 'Sigup'
-
-@auth.route('/logout')
-def logout():
-    return 'Logout'
+    return "BAD LOGIN"

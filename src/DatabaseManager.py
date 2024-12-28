4a18752bbf490eeb65e1c952589516e47ec1a5ad
Taky0ndon <mmaggio89@gmail.com>
2024-12-27 19:29:36 -0500

    added db path parameter to get_db

--- src/DatabaseManager.py
+++ src/DatabaseManager.py
@@ -18,10 +18,10 @@ COLUMNS =  [
         ]
 
 
-def get_db():
+def get_db(db_path=DATABASE):
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE, check_same_thread=False)
+        db = g._database = sqlite3.connect(db_path, check_same_thread=False)
     db.row_factory = sqlite3.Row
     return db
 

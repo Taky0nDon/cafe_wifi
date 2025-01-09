94e9bb9c19dae2fe61f14789dfa9fbd6cad5f444
Taky0ndon <mmaggio89@gmail.com>
2025-01-08 19:05:31 -0500

    altered admin_only decorator so it just checks the User object attribute is_admin, reversed if statements so it checks if the request method is POST instead of GET

--- src/auth.py
+++ src/auth.py
@@ -5,41 +5,35 @@ from functools import wraps
 
 from forms import LoginForm
 from DatabaseManager import get_db
-from UserManager import get_users, User
+from UserManager import get_users, User, user_is_admin
 
 auth = Blueprint('auth', __name__)
 app = Flask("app")
 with app.app_context():
     db = get_db()
 
-def admin_only(function):
+def admin_only(function):  # This is a decorator.
     @wraps(function)
     def view(*args, **kwargs):
-        if flask_login.current_user.is_anonymous or flask_login.current_user.id != "ser03":
-            return abort(code=403)
-        else:
+        if flask_login.current_user.is_admin:
             return function(*args, **kwargs)
+        else:
+            return abort(code=403)
     return view
 
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
 
 @auth.route('/login', methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    if request.method == "GET":
-        return render_template("login.html", form=login_form)
+    if request.method == "POST":
+        username = request.form["username"]
+        users = get_users()
+        if username in users and request.form["password"] == users[username]["pw_hash"]:
+            print(f"Creating a user objext in login")
+            user = User()
+            user.id = username
+            flask_login.login_user(user)
+            return redirect(url_for("admin_welcome"))
+        return "BAD LOGIN"
+    return render_template("login.html", form=login_form)
 
-    username = request.form["username"]
-    users = get_users()
-    if username in users and request.form["password"] == users[username]["pw_hash"]:
-        user = User()
-        user.id = username
-        flask_login.login_user(user)
-        return redirect(url_for("admin_welcome"))
-
-    return "BAD LOGIN"

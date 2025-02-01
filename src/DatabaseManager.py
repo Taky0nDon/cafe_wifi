15d46dd84c3dbe78de5c7a482d0844e19ffdade3
Taky0ndon <mmaggio89@gmail.com>
2025-01-31 21:57:19 -0500

    table to insert rows into can now be passed as an argument to the insert() function, default value is cafe

--- src/DatabaseManager.py
+++ src/DatabaseManager.py
@@ -41,12 +41,12 @@ def build_query(column='*')-> str:
     query = f"SELECT {column} from cafes"
     return query
 
-def insert(row: dict, db: sqlite3.Connection) -> None:
-    initial_rows = query_db("SELECT Count(*) FROM cafe")
+def insert(row: dict, db: sqlite3.Connection, table: str="cafe") -> None:
+    initial_rows = query_db(f"SELECT Count(*) FROM {table}")
     if initial_rows is not None:
         count = initial_rows[0]
     c = db.cursor()
-    statement = f'insert into cafe values ({"?, "*len(row) +"?"})'
+    statement = f'insert into {table} values ({"?, "*len(row) +"?"})'
     c.execute(statement, [count[0]+1] + [r for r in row.values()]
                )
     db.commit()

e2fedf3390f769f5cac99cddb8f0d302bcc3287b
Taky0ndon <mmaggio89@gmail.com>
2025-01-31 21:55:57 -0500

    removed send_email function, since now the admin will approve submissions via the website directly

--- src/MailHandler.py
+++ src/MailHandler.py
@@ -4,18 +4,16 @@ from smtplib import SMTP
 
 from dotenv import load_dotenv
 
+
+from DatabaseManager import insert
+
 class Message:
     def __init__(self, subject:str , content: str):
         self.message = f"Subject:{subject}\n\n{content}"
 
 
-def send_cafe_request_email(smtp_server: str, email_address: str,
-                            sender_auth: str, message: str):
-    with SMTP(smtp_server) as connection:
-        connection.starttls()
-        connection.login(email_address, sender_auth)
-        connection.sendmail(from_addr=email_address, to_addrs=email_address,
-                            msg=message)
+def submit_request(request_data):
+    insert(request_data,
 
 if __name__ == "__main__":
     load_dotenv(".env")

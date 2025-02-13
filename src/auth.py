20d95d7e9873209d8f8309ac81c1eb6832428f30
Taky0ndon <mmaggio89@gmail.com>
2025-02-12 19:32:48 -0500

    create dictionary is user_dict to store data to insrt into TABLE user

--- src/auth.py
+++ src/auth.py
@@ -4,7 +4,7 @@ from functools import wraps
 
 
 from forms import LoginForm
-from DatabaseManager import get_db
+from DatabaseManager import get_db, insert
 from UserManager import get_users, User, user_is_admin
 
 auth = Blueprint('auth', __name__)
@@ -22,18 +22,39 @@ def admin_only(function):  # This is a decorator.
     return view
 
 
+def create_user_object(username: str) -> User:
+            user = User()
+            user.id = username
+            return user
+
+
 @auth.route('/login', methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
     if request.method == "POST":
         username = request.form["username"]
         users = get_users()
+        print(users)
         if username in users and request.form["password"] == users[username]["pw_hash"]:
-            print(f"Creating a user objext in login")
-            user = User()
-            user.id = username
-            flask_login.login_user(user)
+            flask_login.login_user(create_user_object(username))
             return redirect(url_for("admin_welcome"))
         return "BAD LOGIN"
     return render_template("login.html", form=login_form)
 
+@auth.route('/register', methods=['GET', 'POST'])
+def register_user():
+    registration_form = LoginForm()
+    if request.method == 'POST':
+        username = request.form['username']
+        if  username in get_users():
+            return "That name is not available."
+        new_user = create_user_object(username)
+        user_dict = {
+               'id': len(get_users()) or 0,
+               'username': request.form['username'],
+               'password': request.form['password']
+                }
+        flask_login.login_user(new_user)
+        insert(user_dict, db=db, table='user')
+
+    return render_template("register.html")

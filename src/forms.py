ee16eddc775634e20880334b1f98f6666d1ae90d
Taky0ndon <mmaggio89@gmail.com>
2025-02-21 21:01:27 -0500

    - added PendingCafeForm - did something stupid in DeleteCafeForm. I think I thought I was in   PendingCafeForm

--- src/forms.py
+++ src/forms.py
@@ -1,56 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import HiddenField, PasswordField, StringField, SubmitField, validators
+from wtforms import HiddenField, PasswordField, RadioField, StringField, SubmitField
 from wtforms.fields.choices import SelectField
 from wtforms.validators import DataRequired
 
-from DatabaseManager import get_all_cafes
-
 class LoginForm(FlaskForm):
-    username = StringField("Username: ",
-                           validators=[DataRequired()])
-    password = PasswordField("Password: ",
-                             validators=[DataRequired()])
+    username = StringField("Username: ", validators=[DataRequired()])
+    password = PasswordField("Password: ", validators=[DataRequired()])
     submit = SubmitField("Login")
-
 
 class RegisterForm(LoginForm):
     submit = SubmitField("Sign up")
 
-
+#TODO booleans should be radio fields
 class AddCafeForm(FlaskForm):
-    name = StringField("Name: ",
-                            validators=[DataRequired()]
-                            )
-    map_url = StringField("Map URL: ",
-                            validators=[DataRequired()]
-                            )
-    img_url = StringField("Image URL: ",
-                            validators=[DataRequired()]
-                            )
-    location = StringField("Location: ",
-                            validators=[DataRequired()]
-                            )
-    has_sockets = StringField("Sockets (Y/N): ",
-                            validators=[DataRequired()]
-                              )
-    has_toilet = StringField("Toilet (Y/N): ",
-                            validators=[DataRequired()]
-                              )
-    has_wifi = StringField("WiFi (Y/N): ",
-                            validators=[DataRequired()]
-                              )
-    can_take_calls = StringField("Calls (Y/N): ",
-                            validators=[DataRequired()]
-                              )
-    seats = StringField("Number of seats: ",
-                            validators=[DataRequired()]
-                              )
-    coffee_price = StringField("Coffee Price: ",
-                            validators=[DataRequired()]
-                              )
+    name = StringField("Name: ", validators=[DataRequired()])
+    map_url = StringField("Map URL: ", validators=[DataRequired()])
+    img_url = StringField("Image URL: ", validators=[DataRequired()])
+    location = StringField("Location: ", validators=[DataRequired()])
+    has_sockets = StringField("Sockets (Y/N): ", validators=[DataRequired()])
+    has_toilet = StringField("Toilet (Y/N): ", validators=[DataRequired()])
+    has_wifi = StringField("WiFi (Y/N): ", validators=[DataRequired()])
+    can_take_calls = StringField("Calls (Y/N): ", validators=[DataRequired()])
+    seats = StringField("Number of seats: ", validators=[DataRequired()])
+    coffee_price = StringField("Coffee Price: ", validators=[DataRequired()])
     submit = SubmitField("Add Cafe")
     submitted_by_id = HiddenField("placeholder")
 
 class DeleteCafeForm(FlaskForm):
-    cafe_name = SelectField()
+    cafe_name = SelectField(label="Accept?", choices=[("Yes", "accept"), ("No", "reject")])
     submit = SubmitField("Remove this cafe")
+
+class PendingCafeForm(FlaskForm):
+    accept = RadioField()
